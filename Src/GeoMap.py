f130c9e8d0f994ea0a1ca5eac155e3e29694dce9
tjadejong <tjadejong@thinkpractice.nl>
2017-08-09 16:48:19 +0200

    Adapted ImportFoto to use GeoMap

--- Src/GeoMap.py
+++ Src/GeoMap.py
@@ -1,5 +1,6 @@
-import gdal
 from gdalconst import *
+import gdal
+import numpy
 
 class GeoMap(object):
     def __init__(self, dataset):
@@ -15,13 +16,58 @@ class GeoMap(object):
 
     @property
     def widthInPixels(self):
+        """Returns the width in pixels of the opened raster map
+
+        :return: the width in pixels
+
+        :rtype: int
+        """
         return self.dataset.RasterXSize
 
     @property
     def heightInPixels(self):
+        """Returns the height in pixels of the opened raster map
+
+        :return: the height in pixels
+
+        :rtype: int
+        """
         return self.dataset.RasterYSize
 
     @classmethod
     def open(cls, filename):
+        """Opens a GeoTiff file with filename and returns a GeoMap object for it
+
+        :param str filename: the filename of the GeoTiff file to open
+
+        :return: a GeoMap object for the GeoTiff file
+
+        :rtype: GeoMap
+        """
         dataset = gdal.Open(filename, GA_ReadOnly)
         return GeoMap(dataset)
+
+    def readTile(self, startX, startY, width, height):
+        """
+
+        :param int startX:
+        :param int startY:
+        :param int width:
+        :param int height:
+
+        :return:
+        """
+        R = self.dataset.GetRasterBand(1)
+        G = self.dataset.GetRasterBand(2)
+        B = self.dataset.GetRasterBand(3)
+
+        redArray = R.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
+        greenArray = G.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
+        blueArray = B.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
+
+        redArray = numpy.expand_dims(redArray, axis=2)
+        greenArray = numpy.expand_dims(greenArray, axis=2)
+        blueArray = numpy.expand_dims(blueArray, axis=2)
+
+        stacked = numpy.append(redArray, greenArray, axis=2)
+        return numpy.append(stacked, blueArray, axis=2)
--- Src/ImportFoto.py
+++ Src/ImportFoto.py
@@ -1,41 +1,15 @@
-import gdal
 import matplotlib.pyplot as plt
-import numpy
-from gdalconst import *
+from Src.GeoMap import GeoMap
 
+geoMap = GeoMap.open(r"/home/tjadejong/Documents/CBS/ZonnePanelen/Parkstad.tif")
 
-def readGeoImage(dataset, startX, startY, width, height):
-    R = dataset.GetRasterBand(1)
-    G = dataset.GetRasterBand(2)
-    B = dataset.GetRasterBand(3)
+#geotransform = dataset.GetGeoTransform()
+#print (geotransform)
 
-    redArray = R.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
-    greenArray = G.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
-    blueArray = B.ReadAsArray(xoff=startX, yoff=startY, win_xsize=width, win_ysize=height)
+print ("width={}, height={}".format(geoMap.widthInPixels, geoMap.heightInPixels))
 
-    redArray = numpy.expand_dims(redArray, axis=2)
-    greenArray = numpy.expand_dims(greenArray, axis=2)
-    blueArray = numpy.expand_dims(blueArray, axis=2)
-
-    stacked = numpy.append(redArray, greenArray, axis=2)
-    return numpy.append(stacked, blueArray, axis=2)
-
-
-dataset = gdal.Open(r"/home/tjadejong/Documents/CBS/ZonnePanelen/Parkstad.tif", GA_ReadOnly)
-
-if dataset is None:
-    print("Could not read dataset")
-
-geotransform = dataset.GetGeoTransform()
-print (geotransform)
-
-rasterXSize = dataset.RasterXSize
-rasterYSize = dataset.RasterYSize
-print ("width={}, height={}".format(rasterXSize, rasterYSize))
-
-
-coverageInM = (dataset.RasterXSize * abs(geotransform[1]) + dataset.RasterYSize * abs(geotransform[5]))
-print(coverageInM)
+#coverageInM = (dataset.RasterXSize * abs(geotransform[1]) + dataset.RasterYSize * abs(geotransform[5]))
+#print(coverageInM)
 
 tileWidth = 300
 numRows = 2
@@ -45,6 +19,6 @@ for row in range(numRows):
     y = row * tileWidth
     for column in range(numColumns):
         x = column * tileWidth
-        stacked = readGeoImage(dataset, x, y, 300, 300)
+        stacked = geoMap.readTile(x, y, 300, 300)
         axarr[row, column].imshow(stacked, interpolation='nearest', vmin=0)
 plt.show()
