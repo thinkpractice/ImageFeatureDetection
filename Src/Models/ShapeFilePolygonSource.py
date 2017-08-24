f4f5ef4c09ab43b8c012a344c137a921ca472a09
tjadejong <tjadejong@thinkpractice.nl>
2017-08-24 10:45:53 +0200

    Added some methods to ShapeFilePolygonSource. Now it should be possible to get the polygons from a shape file.

--- Src/Models/ShapeFilePolygonSource.py
+++ Src/Models/ShapeFilePolygonSource.py
@@ -12,6 +12,16 @@ class ShapeFilePolygonSource(PolygonSource):
             shapeBoundingBox = buildingShape.bbox
             if self.withinBox(shapeBoundingBox, gpsBoundingBox):
                 buildings.append(buildingShape)
+        self.polygons = self.getPolygonsFor(self.geoTileCollection, buildings)
 
     def withinBox(self, shapeBbox, gpsBbox):
-        return False
+        return (shapeBbox[0] >= gpsBbox[0] or shapeBbox[2] <= gpsBbox[2]) and \
+                (shapeBbox[1] >= gpsBbox[1] or shapeBbox[3] <= gpsBbox[3])
+
+    def getPolygonsFor(self, geoTileCollection, buildings):
+        for building in buildings:
+            imageId = building.PNDID
+            polygonCoordinates = self.getPolygonCoordinatesFromList(geoTileCollection,[[node.lon, node.lat] for node in building.points])
+            if not polygonCoordinates:
+                continue
+            yield imageId, polygonCoordinates
