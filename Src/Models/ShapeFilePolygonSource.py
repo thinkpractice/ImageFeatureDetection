d6ff30c9dc1eedb7a27805158cabb2b9103cb5f6
tjadejong <tjadejong@thinkpractice.nl>
2017-09-06 14:45:37 +0200

    Added PlotHistogram and updated path to new shape file for buildings

--- Src/Models/ShapeFilePolygonSource.py
+++ Src/Models/ShapeFilePolygonSource.py
@@ -7,7 +7,7 @@ class ShapeFilePolygonSource(PolygonSource):
 
     def query(self, gpsBoundingBox):
         buildings = []
-        buildingShapes = shapefile.Reader(r"/home/tjadejong/Documents/CBS/ZonnePanelen/Shapes/pand_parkstad_wgs84")
+        buildingShapes = shapefile.Reader(r"/home/tjadejong/Documents/CBS/ZonnePanelen/Shapes/panden_parkstad_2016_dis")
         self.numberOfPolygons = buildingShapes.numRecords
         for buildingShape in buildingShapes.iterShapeRecords():
             shapeBoundingBox = buildingShape.shape.bbox
