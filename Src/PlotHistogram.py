366254fba116910243207492c814d0c64bea141d
tjadejong <tjadejong@thinkpractice.nl>
2017-09-11 10:45:17 +0200

    Refactored code of PlotHistogram a bit in preparation of histogram split

--- Src/PlotHistogram.py
+++ Src/PlotHistogram.py
@@ -1,27 +1,47 @@
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 import csv
+import sys
 
-histogram = []
-with open(r"/home/tjadejong/Documents/CBS/ZonnePanelen/histogram.csv", 'r') as csvFile:
-    csvReader = csv.reader(csvFile)
-    for row in csvReader:
-        r, g, b, v = row
-        histogram.append((r,g,b))
+def loadHistogramFile(filename):
+    histogram = []
+    with open(filename, 'r') as csvFile:
+        csvReader = csv.reader(csvFile)
+        for row in csvReader:
+            r, g, b, v = row
+            histogram.append((int(r), int(g), int(b)))
+    return histogram
+
+def plotHistogram(histogram):
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    r = []
+    g = []
+    b = []
+    colors = []
+    for row in histogram:
+        x, y, z = row
+        r.append(x)
+        g.append(y)
+        b.append(z)
+        colors.append([value / 255 for value in row])
+    ax.scatter(r, g, b, c=colors)
+    pyplot.show()
+
+def main(args):
+    if len(args) < 2:
+        print("usage: PlotHistogram <input file.csv> [#histograms]")
+        exit(1)
+    if len(args) == 3:
+        numberOfHistograms = int(args[2])
+    histogram = loadHistogramFile(args[1])
+    plotHistogram(histogram)
 
 
-fig = pyplot.figure()
-ax = Axes3D(fig)
+if __name__ == "__main__":
+    args = sys.argv
+    main(args)
 
-r = []
-g = []
-b = []
-colors = []
-for row in histogram:
-    x,y,z = row
-    r.append(x)
-    g.append(y)
-    b.append(z)
-    colors.append(row)
-ax.scatter(r, g, b) #, color=colors)
-pyplot.show()
+
+
+
