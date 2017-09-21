9d13643b8f773f14421bc732953b8c8ebf938e59
tjadejong <tjadejong@thinkpractice.nl>
2017-09-21 17:12:33 +0200

    Added code to calculate some standard image statistics like mean, variance and covariance

---
+++ Src/CalculateImageStatistics.py
@@ -0,0 +1,21 @@
+from skimage.io import imread
+import sys
+import ImageStatistics
+
+def main(argv):
+    if len(argv) < 2:
+        print ("usage: ImageStatistics.py <filename>")
+        exit(1)
+
+    image = imread(argv[1])
+    print("std1={}".format(ImageStatistics.rgbImageVariance(image)))
+    normalizedImage = ImageStatistics.normalizeImage(image)
+    print("std2={}".format(ImageStatistics.rgbImageVariance(normalizedImage)))
+    print("imageMeans={}".format(ImageStatistics.rgbImageMean(normalizedImage)))
+    print("imageVariance={}".format(ImageStatistics.rgbImageVariance(normalizedImage)))
+    print("image covariance matrix={}".format(ImageStatistics.rgbImageCovarianceMatrix(normalizedImage)))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
---
+++ Src/ImageStatistics.py
@@ -0,0 +1,45 @@
+import numpy as np
+import math
+
+def normalizeImage(image):
+    return image / 255
+
+def imageMean(image):
+    return (1 / image.size) * np.sum(image)
+
+def centerImage(image):
+    return image - imageMean(image)
+
+def imageCovariance(image1, image2):
+    return (1 / image1.size) * np.dot(centerImage(image1).ravel(), centerImage(image2).ravel())
+
+def imageVariance(image):
+    return imageCovariance(image, image)
+
+def linearCorrelation(image1, image2):
+    return imageCovariance(image1, image2) / math.sqrt(imageVariance(image1) * imageVariance(image2))
+
+def rgbApply(image, function):
+    return [function(image[:,:,i]) for i in range(3)]
+
+def rgbImageMean(image):
+    return rgbApply(image, lambda imageBand: imageMean(imageBand))
+
+def rgbCenterImage(image):
+    return np.stack(rgbApply(image, lambda imageBand: centerImage(imageBand)))
+
+def rgbImageVariance(image):
+    return rgbApply(image, lambda imageBand: imageVariance(imageBand))
+
+def rgbImageCovarianceMatrix(rgbImage):
+    covarianceMatrix = np.zeros([3,3])
+    for r in range(3):
+        for c in range (3):
+            covariance = imageCovariance(rgbImage[:,:,r], rgbImage[:,:,c])
+            covarianceMatrix[r, c] = covariance
+            covarianceMatrix[c, r] = covariance
+    return covarianceMatrix
+
+
+def fractionOfInformation(rgbImage):
+    pass
