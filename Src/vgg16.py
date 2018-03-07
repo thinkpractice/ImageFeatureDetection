586c5f60980f9bf57d1b64c050a52f19ed315909
Tim De Jong <tjadejong@thinkpractice.nl>
2018-03-07 09:28:53 +0100

    An cnn infrastructure with more stable performance accros the line

--- Src/vgg16.py
+++ Src/vgg16.py
@@ -46,27 +46,12 @@ def VGG_16(weights_path=None):
     #model.add(Conv2D(256, (3, 3), activation='relu'))
     #model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(ZeroPadding2D((1,1)))
-    #model.add(Conv2D(512, (3, 3), activation='relu'))
-    #model.add(MaxPooling2D((2,2), strides=(2,2)))
-
     model.add(Flatten())
-    model.add(Dense(2048, activation='relu'))
+    #model.add(Dense(2048, activation='relu'))
+    model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
-    #model.add(Dense(512, activation='relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(1, activation="sigmoid"))
 
     if weights_path:
